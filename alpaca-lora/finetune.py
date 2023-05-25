adeb02540ddc75a811a5e9bd37acda92edbc196d
azayz <azizbellaweid@hotmail.com>
2023-05-25 14:06:01 +0200

    feat: log params

--- alpaca-lora/finetune.py
+++ alpaca-lora/finetune.py
@@ -53,8 +53,8 @@ def train(
     prompt_template_name: str = "alpaca",  # The prompt template to use, will default to alpaca.
     debug: bool = False,
     # debug mode this put all other parameters to a really low value so that we can quickly figure out if the code is running proprely or not
-    eval_file: str = "",
-    eval_limit: int = 0,
+    eval_file: str = "",  # path to file you want to evaluate on
+    eval_limit: int = 0,  # limit the number of instructions to evaluate on
 ):
     if debug:
         batch_size = 2
@@ -63,31 +63,32 @@ def train(
         num_epochs = 1
 
     if int(os.environ.get("LOCAL_RANK", 0)) == 0:
-        print(
-            f"Training Alpaca-LoRA model with params:\n"
-            f"base_model: {base_model}\n"
-            f"data_path: {data_path}\n"
-            f"output_dir: {output_dir}\n"
-            f"batch_size: {batch_size}\n"
-            f"micro_batch_size: {micro_batch_size}\n"
-            f"num_epochs: {num_epochs}\n"
-            f"learning_rate: {learning_rate}\n"
-            f"cutoff_len: {cutoff_len}\n"
-            f"val_set_size: {val_set_size}\n"
-            f"lora_r: {lora_r}\n"
-            f"lora_alpha: {lora_alpha}\n"
-            f"lora_dropout: {lora_dropout}\n"
-            f"lora_target_modules: {lora_target_modules}\n"
-            f"train_on_inputs: {train_on_inputs}\n"
-            f"add_eos_token: {add_eos_token}\n"
-            f"group_by_length: {group_by_length}\n"
-            f"wandb_project: {wandb_project}\n"
-            f"wandb_run_name: {wandb_run_name}\n"
-            f"wandb_watch: {wandb_watch}\n"
-            f"wandb_log_model: {wandb_log_model}\n"
-            f"resume_from_checkpoint: {resume_from_checkpoint or False}\n"
-            f"prompt template: {prompt_template_name}\n"
-        )
+        params_dict = {
+            "base_model": base_model,
+            "data_path": data_path,
+            "output_dir": output_dir,
+            "batch_size": batch_size,
+            "micro_batch_size": micro_batch_size,
+            "num_epochs": num_epochs,
+            "learning_rate": learning_rate,
+            "cutoff_len": cutoff_len,
+            "val_set_size": val_set_size,
+            "lora_r": lora_r,
+            "lora_alpha": lora_alpha,
+            "lora_dropout": lora_dropout,
+            "lora_target_modules": lora_target_modules,
+            "train_on_inputs": train_on_inputs,
+            "add_eos_token": add_eos_token,
+            "group_by_length": group_by_length,
+            "wandb_project": wandb_project,
+            "wandb_run_name": wandb_run_name,
+            "wandb_watch": wandb_watch,
+            "wandb_log_model": wandb_log_model,
+            "resume_from_checkpoint": resume_from_checkpoint or False,
+            "prompt template": prompt_template_name,
+        }
+
+        print("Training Alpaca-LoRA model with params:", params_dict)
 
     assert (
         base_model
@@ -111,6 +112,7 @@ def train(
     if len(wandb_project) > 0:
         os.environ["WANDB_PROJECT"] = wandb_project
         run = wandb.init(wandb_project)
+        run.log(columns=list(params_dict.keys()), data=list(params_dict.values()))
     if len(wandb_watch) > 0:
         os.environ["WANDB_WATCH"] = wandb_watch
     if len(wandb_log_model) > 0:
