b52116e5896e2b407daeb4278e87d2090a3f83a5
Sebastian Weisshaar <76220851+sebastian-weisshaar@users.noreply.github.com>
2023-05-26 13:49:39 +0200

    style: remove unnecessary print
    Co-authored-by: samsja <55492238+samsja@users.noreply.github.com>

--- alpaca-lora/finetune.py
+++ alpaca-lora/finetune.py
@@ -286,7 +286,6 @@ def train(
 
     if torch.__version__ >= "2" and sys.platform != "win32":
         model = torch.compile(model)
-    print('WANDB3', use_wandb)
     trainer.train(resume_from_checkpoint=resume_from_checkpoint)
 
     model.save_pretrained(output_dir)
