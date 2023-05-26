b597cba411cf255b5b6c22e675445e50f079ab29
sebastian-weisshaar <sebastian.v.weisshaar@gmail.com>
2023-05-26 12:02:02 +0200

    fix: removed mps again

--- alpaca-lora/evaluate.py
+++ alpaca-lora/evaluate.py
@@ -12,11 +12,6 @@ if torch.cuda.is_available():
     device = "cuda"
 else:
     device = "cpu"
-try:
-    if torch.backends.mps.is_available():
-        device = "mps"
-except:  # noqa: E722
-    pass
 
 
 def evaluate(
