e025d6be94aa05420b8823162ec994daea43dae7
sebastian-weisshaar <sebastian.v.weisshaar@gmail.com>
2023-06-01 17:56:33 +0200

    fix: changed names in evaluate and in test

--- alpaca-lora/evaluate.py
+++ alpaca-lora/evaluate.py
@@ -76,10 +76,10 @@ def evaluate(
                 "instruction": eval_instance["instruction"],
                 "input": eval_instance["instances"][0]["input"],
                 "output": output,
-                "alpaca_lora_output": eval_instance["instances"][0]["output"],
-                "stanford_alpaca_output": eval_instance["instances"][0][
+                "alpaca_lora_output": eval_instance["instances"][0][
                     "stanford_alpaca_output"
                 ],
+                "human_evaluation": eval_instance["instances"][0]["output"],
             }
         )
     return results
