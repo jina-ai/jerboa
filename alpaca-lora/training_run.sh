03659c65abebc0bcf6e04a025a1f694b11ca8875
sebastian-weisshaar <sebastian.v.weisshaar@gmail.com>
2023-06-12 11:53:25 +0200

    feat: script to execute training run and stop runpod afterwards

---
+++ alpaca-lora/training_run.sh
@@ -0,0 +1,20 @@
+
+# use this script by passing the training argument as string
+# Example:
+ :' ./executor "python finetune.py  \
+  --base_model yahma/llama-7b-hf \
+  --output_dir ./lora-alpaca \
+  --batch_size 128 \
+  --micro_batch_size 4 \
+  --eval_limit 30 -\
+  -eval_file eval.json \
+  --wandb_log_model true  \
+  --wandb_watch gradients \
+  --num_epochs 3"
+'
+echo starting run
+eval "$1"
+echo training done
+
+#Stop the runpod instance
+runpodctl stop pod $RUNPOD_POD_ID
