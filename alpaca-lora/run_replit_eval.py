b3c537155ff1ed592290ea99df46cff579264881
azayz <azizbellaweid@hotmail.com>
2023-06-14 17:04:39 +0200

    chore: black

--- alpaca-lora/run_replit_eval.py
+++ alpaca-lora/run_replit_eval.py
@@ -1,9 +1,14 @@
 import json
-from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch
 
-tokenizer = AutoTokenizer.from_pretrained("teknium/Replit-v2-CodeInstruct-3B", trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained("teknium/Replit-v2-CodeInstruct-3B", trust_remote_code=True)
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained(
+    "teknium/Replit-v2-CodeInstruct-3B", trust_remote_code=True
+)
+model = AutoModelForCausalLM.from_pretrained(
+    "teknium/Replit-v2-CodeInstruct-3B", trust_remote_code=True
+)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 eval_data = []
@@ -13,13 +18,35 @@ with open('code_eval.jsonl', 'r') as f:
 
 results = []
 for i, eval_instance in enumerate(eval_data):
-    x = tokenizer.encode("### Instruction: \n" + eval_instance['instruction']+ "### Input: \n" + eval_instance["instances"][0]["input"] + "### Response: \n", return_tensors='pt')
+    x = tokenizer.encode(
+        "### Instruction: \n"
+        + eval_instance['instruction']
+        + "### Input: \n"
+        + eval_instance["instances"][0]["input"]
+        + "### Response: \n",
+        return_tensors='pt',
+    )
     x = x.to(device)
     print(x.is_cuda)
     print(next(model.parameters()).is_cuda)
-    y = model.generate(x, max_length=256, do_sample=True, top_p=0.95, top_k=4, temperature=0.2, num_return_sequences=1, eos_token_id=tokenizer.eos_token_id)
-    print(tokenizer.decode(y[0], skip_special_tokens=True, clean_up_tokenization_spaces=False))
-    eval_instance["instances"][0]["replit_code_output"] = tokenizer.decode(y[0], skip_special_tokens=True, clean_up_tokenization_spaces=False)
+    y = model.generate(
+        x,
+        max_length=256,
+        do_sample=True,
+        top_p=0.95,
+        top_k=4,
+        temperature=0.2,
+        num_return_sequences=1,
+        eos_token_id=tokenizer.eos_token_id,
+    )
+    print(
+        tokenizer.decode(
+            y[0], skip_special_tokens=True, clean_up_tokenization_spaces=False
+        )
+    )
+    eval_instance["instances"][0]["replit_code_output"] = tokenizer.decode(
+        y[0], skip_special_tokens=True, clean_up_tokenization_spaces=False
+    )
     results.append(eval_instance)
 
 with open('code_eval.jsonl', 'w') as f:
