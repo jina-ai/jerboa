a971178c00e593e5e565f0774454d7ec2319aeb9
Sebastian Weisshaar <sebastian.weisshaar@jina.ai>
2023-06-29 10:59:51 +0200

    fix: only use ddp if we have more than 1 GPU

--- jerboa/finetune.py
+++ jerboa/finetune.py
@@ -169,7 +169,7 @@ def train(
 
     device_map = "auto"
     world_size = int(os.environ.get("WORLD_SIZE", 1))
-    ddp = world_size != 1
+    ddp = world_size > 1
     if ddp:
         device_map = {"": int(os.environ.get("LOCAL_RANK") or 0)}
         gradient_accumulation_steps = gradient_accumulation_steps // world_size
