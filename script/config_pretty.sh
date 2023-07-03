90de0e96acbd9624d9af4e1fc2b697868a4e8156
samsja <sami.jaghouar@hotmail.fr>
2023-07-03 11:46:56 +0200

    feat: add pretty config scriot
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>

---
+++ script/config_pretty.sh
@@ -0,0 +1,22 @@
+# Setup of runpod instance
+apt update -y
+apt install gh screen neovim nvtop -y
+pip install -U poetry
+gh auth login
+gh repo clone jina-ai/jerboa
+cd jerboa
+pwd
+poetry config virtualenvs.create false \
+     && poetry install --no-interaction --no-ansi
+
+poetry run pip install torch
+
+
+curl -sS https://starship.rs/install.sh | sh 
+echo 'eval "$(starship init bash)"' >> ~/.bashrc
+source .bashrc
+
+wandb login
+screen -S training_screen
+
+
