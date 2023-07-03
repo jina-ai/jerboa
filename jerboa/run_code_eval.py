c03d154026163b679713f63f5f62ad28015407e3
Sebastian Weisshaar <sebastian.weisshaar@jina.ai>
2023-07-03 13:14:31 +0000

    feat: make code understandable

--- jerboa/run_code_eval.py
+++ jerboa/run_code_eval.py
@@ -9,6 +9,7 @@ from transformers import (
     BitsAndBytesConfig,
     GenerationConfig,
 )
+from jerboa.utils.prompter import Prompter
 
 
 app = Typer(pretty_exceptions_enable=False)
@@ -35,12 +36,6 @@ tokenizer = AutoTokenizer.from_pretrained(
 )
 
 
-# with torch.no_grad():
-#   outputs = model.generate(input_ids=inputs["input_ids"].to("cuda"), max_new_tokens=10)
-#   print(tokenizer.batch_decode(outputs.detach().cpu().numpy(), skip_special_tokens=True)[0])
-# # 'complaint'
-
-
 @app.command()
 def run_eval(eval_file: str = "eval.jsonl"):
     eval_data = []
@@ -49,47 +44,51 @@ def run_eval(eval_file: str = "eval.jsonl"):
             eval_data.append(json.loads(line))
 
     results = []
-    targets = [18, 19, 20, 21, 22]
+    targets = list(range(3))
+    # Create tokenized data
+    # Create prompts
+    prompts = []
     for eval_instance in map(eval_data.__getitem__, targets):
-        x = tokenizer(
+        prompts.append(
             "### Instruction: \n"
             + eval_instance['instruction']
             + "### Input: \n"
             + eval_instance["instances"][0]["input"]
-            + "### Response: \n",
-            return_tensors='pt',
-        ).to(device)
-        GEN_CONFIG_PATH = 'tiiuae/falcon-7b'
-        GenerationConfig.from_pretrained(GEN_CONFIG_PATH)
-        with torch.no_grad():
-            y = model.generate(
-                input_ids=x['input_ids'],
-                attention_mask=x['attention_mask'],
-                # generation_config=generation_config,
-                max_length=1048,
-                do_sample=True,
-                top_p=0.95,
-                top_k=4,
-                temperature=0.2,
-                num_return_sequences=1,
-                eos_token_id=tokenizer.eos_token_id,
-                pad_token_id=tokenizer.eos_token_id,
-                repetition_penalty=2.0,
-                length_penalty=-100,
-            )
-        print(
-            tokenizer.decode(
-                y[0], skip_special_tokens=True, clean_up_tokenization_spaces=False
-            )
+            + "### Response: \n"
         )
-        eval_instance["instances"][0]["starcoder_code_output"] = tokenizer.decode(
+
+    tokenized_prompts = tokenizer(
+        prompts,
+        return_tensors='pt',
+    )
+
+    GEN_CONFIG_PATH = 'tiiuae/falcon-7b'
+    GenerationConfig.from_pretrained(GEN_CONFIG_PATH)
+    with torch.no_grad():
+        y = model.generate(
+            input_ids=tokenized_prompts['input_ids'],
+            attention_mask=tokenized_prompts['attention_mask'],
+            #generation_config=generation_config,
+            max_length=1048,
+            do_sample=True,
+            top_p=0.95,
+            top_k=4,
+            temperature=0.2,
+            num_return_sequences=1,
+            eos_token_id=tokenizer.eos_token_id,
+            pad_token_id=tokenizer.eos_token_id,
+            repetition_penalty=2.0,
+            length_penalty=-100,
+        )
+
+        result = tokenizer.decode(
             y[0], skip_special_tokens=True, clean_up_tokenization_spaces=False
         )
-        results.append(eval_instance)
 
-    # with open(eval_file, 'w') as f:
-    #     for result in results:
-    #         f.write(json.dumps(result) + '\n')
+        pr
+    with open(eval_file, 'w') as f:
+        for result in results:
+            f.write(json.dumps(result) + '\n')
 
 
 if __name__ == "__main__":
