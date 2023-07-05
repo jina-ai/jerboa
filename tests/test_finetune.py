c71e853b6afa7dd2f4576a7c8df498562f526acc
Sebastian Weisshaar <sebastian.weisshaar@jina.ai>
2023-07-05 16:06:27 +0200

    test: remove checks for eval test

--- tests/test_finetune.py
+++ tests/test_finetune.py
@@ -52,8 +52,6 @@ def test_eval():
         assert 'id' in res
         assert 'input' in res
         assert 'output' in res
-        assert 'alpaca_lora_output' in res
-        assert 'human_evaluation' in res
     assert (
         results[0]['instruction']
         == "The sentence you are given might be too wordy, complicated, or unclear. "
