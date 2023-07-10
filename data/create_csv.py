9465ffa2731d2ddcd9f71510915c2faa232399ef
Sebastian Weisshaar <sebastian.weisshaar@jina.ai>
2023-07-10 14:28:03 +0000

    refactor: remove csv creation file

--- data/create_csv.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import json
-
-with open('/root/jerboa/jerboa/eval_corrected.jsonl') as f:
-    data = [json.loads(line) for line in f]
-
-df = pd.DataFrame(data)
-df.to_csv('.comparison.csv')
