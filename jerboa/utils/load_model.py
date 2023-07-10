19a46814639f7eac4a11e7455ae035c856886917
Sebastian Weisshaar <sebastian.weisshaar@jina.ai>
2023-07-10 12:58:42 +0000

    fix: remove typo in load model

--- jerboa/utils/load_model.py
+++ jerboa/utils/load_model.py
@@ -59,7 +59,7 @@ def load_model(
         """
 
         return PeftModel.from_pretrained(
-            base_model=base_model,
+            model=base_model,
             model_id=lora_dir,
         )
 
