edd54b6e7940f0970f9a4886fe441f625af347ce
Sebastian Weisshaar <76220851+sebastian-weisshaar@users.noreply.github.com>
2023-07-11 09:15:31 +0200

    refactor: make general load model function in utils (#105)
    * feat: evaluate first two examples
    
    * feat: trust remote code = True
    
    * feat: add quantization
    
    * feat: correct library imports
    
    * refactor: print(x) for evaluation
    
    * feat: take first element of x for generation
    
    * refactor: device allocation at creation of model
    
    * fix: move model to device not tokenizer
    
    * refactor: remove encode from tokenizer
    
    * feat: put model in eval mode
    
    * refactor: comment out kwargs
    
    * refactor: print(x)
    
    * refactor: print model type
    
    * refactor: change arg to kwarg
    
    * refactor: uncommented generate args
    
    * feat: add generation config
    
    * refactor: add generationconfig import
    
    * feat: get configuation from original model
    
    * fix: typo
    
    * feat: set padding token to eos token
    
    * feat: add penalties for repition and length
    
    * refactor: add generation config back
    
    * refactor: increase length penalty
    
    * refactor: change evaluation file
    
    * feat: increased penalties for length and repition
    
    * feat: extreme penalties
    
    * fix: repitition penalty should be float
    
    * feat: increase max length and length penalty
    
    * refactor: evaluate different instructions
    
    * feat: increase max length
    
    * feat: only get target indices
    
    * feat: lower lentgh and repition penalty
    
    * feat: change back to extreme length and repition penalties
    
    * feat: change back to extreme length and repition penalties
    
    * feat: changed penalties
    
    * style: applied black and ruff
    
    * feat: make code understandable
    
    * feat: create new evaluation file for structure
    
    * feat: organized new generation file, hyperparamters for sensible generation
    
    * feat: create evaluation file
    
    * feat: output file for config adjusted results
    
    * feat: create csv file for comparison
    
    * feat: add typer app
    
    * style: applied black
    
    * feat: unify model loading
    
    * fix: specify lora weights correctly
    
    * test: add test for model loading
    
    * feat: quant config and model config to load model
    
    * feat: remove redundant code in finetune.py
    
    * feat: integrated load_model function into finetuning
    
    * fix: remove typo in load model
    
    * fix: remove typo in load model
    
    * feat: include load model function in save_full_models
    
    * fix: specify correct source of lora weights
    
    * feat: incorporate load model into run_code_eval
    
    * refactor: remove old run_code_eval
    
    * style: apply black and ruff
    
    * refactor: remove eval_corrected.jsonl
    
    * fix: remove main point entry
    
    * feat: include source of lora weights in string
    
    * refactor: remove csv creation file
    
    * refactor: remove csv file
    
    * build: include slow marker in pytoml
    
    * ci: exclude slow parts from ci test
    
    * ci: update flag to exclude slow tests
    
    * ci: change flag for slow test
    
    * ci: remove model loading test
    
    * ci: remove no slow flag from pytest ci
    
    ---------
    
    Co-authored-by: Sebastian Weisshaar <sebastian.weisshaar@jina.ai>

--- jerboa/save_full_models.py
+++ jerboa/save_full_models.py
@@ -1,40 +1,21 @@
-import torch
-from peft import PeftModel
-from transformers import AutoConfig, AutoModelForCausalLM, BitsAndBytesConfig
 from typer import Typer
+from jerboa.utils.load_model import load_model
 
 app = Typer(pretty_exceptions_enable=False)
 
 
 @app.command()
 def save_full_model(
-    base_model: str = "tiiuae/falcon-7b",
-    device_map: str = "auto",
+    base_model: str = 'tiiuae/falcon-7b',
     load_in_4bit: bool = False,
     load_in_8bit: bool = True,
-    lora_weights: str = "artifacts/lora_weight:v12/",
+    lora_weights: str = 'jina-ai/jerboa/lora_weight:v12',
 ):
-    quant_config = BitsAndBytesConfig(
+    model = load_model(
+        base_model,
+        lora_weights,
         load_in_4bit=load_in_4bit,
         load_in_8bit=load_in_8bit,
-        bnb_4bit_use_double_quant=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.float16,
-    )
-
-    model_config = AutoConfig.from_pretrained(base_model, trust_remote_code=True)
-    model = AutoModelForCausalLM.from_pretrained(
-        pretrained_model_name_or_path=base_model,
-        torch_dtype=torch.float16,
-        device_map=device_map,
-        config=model_config,
-        quantization_config=quant_config,
-        trust_remote_code=True,
-    )
-    model = PeftModel.from_pretrained(
-        model,
-        lora_weights,
-        torch_dtype=torch.float16,
     )
     model.base_model.save_pretrained("full_model")
 
